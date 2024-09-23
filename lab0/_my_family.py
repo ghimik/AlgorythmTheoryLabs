a246de8a08a647fc8624fe68ce8a6496061802da
ghimik <bolit006.br0m@gmail.com>
2024-09-23 20:36:24 +0300

    family случайно удалил ;0

--- lab0/_my_family.py
+++ lab0/_my_family.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def calculate_expression():
-    expression1 = (1 + 2) * 3 + 4 + 5
-    expression2 = 1 * (2 + 3) * 4 + 5
-    expression3 = 1 * 2 * 3 + 4 * 5
+def get_family_data():
+    """Получение данных о семье от пользователя."""
+    my_family = []
+    my_family_height = []
 
-    print(f"Результат выражения (1 + 2) * 3 + 4 + 5 = {expression1}. Облом!")
-    print(f"Результат выражения 1 * 2 * 3 + 4 * 5 = {expression3}. Снова облом!")
-    print(f"Результат выражения 1 * (2 + 3) * 4 + 5 = {expression2}. Ура!!")
+    num_members = int(input("Введите количество членов семьи: "))
+
+    for _ in range(num_members):
+        name = input("Введите имя члена семьи: ")
+        height = int(input(f"Введите рост {name} в см: "))
+        my_family.append(name)
+        my_family_height.append([name, height])
+
+    return my_family, my_family_height
+
+
+def print_family_info(my_family_height):
+    """Вывод информации о семье."""
+    father_height = my_family_height[1][1] if len(my_family_height) > 1 else 'не указан'
+    print(f'Рост отца - {father_height} см')
+
+    total_height = sum(member[1] for member in my_family_height)
+    print(f'Общий рост моей семьи - {total_height} см')
+
+
+def main():
+    """Основная функция для отображения информации о семье."""
+    my_family, my_family_height = get_family_data()
+    print_family_info(my_family_height)
 
 
 if __name__ == "__main__":
-    calculate_expression()
+    main()
