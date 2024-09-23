3ca50b5fedd5e96a534b8a049e5ad6bde4397263
ghimik <bolit006.br0m@gmail.com>
2024-09-23 17:59:04 +0300

    универсальность кода в задаче про фильмы

--- lab0/_favorite_movies.py
+++ lab0/_favorite_movies.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+DELIMETER = ','
 # Есть строка с перечислением фильмов
 def show():
     my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
@@ -15,12 +15,21 @@ def show():
     # Использовать .split() или .find()или другие методы строки нельзя - пользуйтесь только срезами,
     # как указано в задании!
 
-    first_movie = my_favorite_movies[:10]
-    last_movie = my_favorite_movies[42:]
-    second_movie = my_favorite_movies[12:25]
-    second_last_movie = my_favorite_movies[35:40]
+    comma_positions = [-2]
+    for i in range(len(my_favorite_movies)):
+        if my_favorite_movies[i] == DELIMETER:
+            comma_positions.append(i)
+    comma_positions.append(len(my_favorite_movies))
 
+    first_movie = my_favorite_movies[:comma_positions[1]]
+    last_movie = my_favorite_movies[comma_positions[-2] + 2:]
+    second_movie = my_favorite_movies[comma_positions[1] + 2:comma_positions[2]]
+    second_last_movie = my_favorite_movies[comma_positions[-3] + 2:comma_positions[-2]]
+
+    # Выводим результаты
     print(first_movie)
     print(last_movie)
     print(second_movie)
     print(second_last_movie)
+
+show()
--- lab0/_my_family.py
+++ lab0/_my_family.py
@@ -18,7 +18,7 @@ def show():
 
     # Выведите на консоль рост отца в формате
     #   Рост отца - ХХ см
-    print(f'Рост отца - {my_family_height[0][1]} см')
+    print(f'Рост отца - {my_family_height[1][1]} см')
 
 
     # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
@@ -26,3 +26,4 @@ def show():
     total_height = sum([member[1] for member in my_family_height])
     print(f'Общий рост моей семьи - {total_height} см')
 
+show()
